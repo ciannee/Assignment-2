cfc856aa5ed3e6adffd80084045ae96f4b91c2d2
Patricia Pineda <pepineda28@gmail.com>
2023-04-06 14:56:42 +0800

    First commit

---
+++ assignment2.py
@@ -0,0 +1,123 @@
+# Name: Pineda, Patricia Anne E.
+# Section: BSCPE 1-5
+# Assignment 2: Problem 2 (Decryption)
+
+# looping
+decryption_str = ""
+while decryption_str != "no":
+   
+# ask for user's input
+    import pyfiglet
+    name = input("\n\033[91m ♡˗ˏ✎*ೃ˚: Hi! Please enter your nickname:  ")
+    hello = "Hello"
+
+    import time
+    time.sleep(3)
+    name_art = pyfiglet.figlet_format(hello + name,font='graffiti')
+    print(name_art)
+
+    input_str = input("\n\033[95mPlease enter a string: ")
+    output_str = ""
+
+    # check each character
+    for i in range(len(input_str)):
+    #   if a, change to *
+        if input_str[i] == "a":
+            output_str += "*"
+    #   if e, change to &
+        elif input_str[i] == "e":
+            output_str += "&"
+    #   if i, change to #
+        elif input_str[i] == "i":
+            output_str += "#"
+    #   if o, change to +
+        elif input_str[i] == "o":
+            output_str += "+"
+    #   if u, change to !
+        elif input_str[i] == "u":
+            output_str += "!"    
+        else:
+            output_str += input_str[i]
+
+    # print output
+
+    import time
+    print("\n⋘  Processing.......... ⋙")
+    time.sleep (3)
+    print("\n██ 20% *___*")
+    time.sleep(1)
+    print("\n███ 40% *___*")
+    time.sleep(1)
+    print("\n████ 60% *___*")
+    time.sleep(1)
+    print("\n█████ 80% *___*")
+    time.sleep(1)
+    print("\n██████ 100% *___*")
+    time.sleep(3)
+
+    print("\nYour encrypted text will be:", output_str)
+
+    import time
+    time.sleep (3)
+
+    # decryption
+
+    print("\nDecrypting your string..........")
+
+    import time
+    print("\n⋘  Processing.......... ⋙")
+    time.sleep (3)
+    print("\n██ 20% *___*")
+    time.sleep(1)
+    print("\n███ 40% *___*")
+    time.sleep(1)
+    print("\n████ 60% *___*")
+    time.sleep(1)
+    print("\n█████ 80% *___*")
+    time.sleep(1)
+    print("\n██████ 100% *___*")
+    time.sleep(3)
+
+    # print output
+    print("\nYour decrypted string will be: \n", input_str, "\n")
+
+
+    # import pyfiglet module
+    import pyfiglet
+    input_art = pyfiglet.figlet_format(input_str, font = "bubble" )
+    print(input_art)
+
+   
+    # looping
+    decryption_str = input("Do you want to try again? (YES or NO): ")
+    if decryption_str == "YES" :
+        print("BEGIN AGAIN.")
+
+        import time
+        print("\n⋘  Processing.......... ⋙")
+        time.sleep (3)
+        print("\n██ 20% *___*")
+        time.sleep(1)
+        print("\n███ 40% *___*")
+        time.sleep(1)
+        print("\n████ 60% *___*")
+        time.sleep(1)
+        print("\n█████ 80% *___*")
+        time.sleep(1)
+        print("\n██████ 100% *___*")
+        time.sleep(3)
+
+    elif decryption_str == "NO" :
+        print("\nSTOP.")
+        import pyfiglet
+        outro = "Thank you!"
+        outro_art = pyfiglet.figlet_format(outro, font = "bulbhead" )
+        import time
+        time.sleep(3)
+        print(outro_art)
+        break
+
+    else: 
+        print("Invalid.")
+       
+        
---
+++ cipher_test.py
@@ -0,0 +1,151 @@
+# Name: Pineda, Patricia Anne E.
+# Section: BSCPE 1-5
+# Assignment 2 : Problem 3 (Vigenere Cipher)
+
+# looping
+
+cipher_str = ""
+while cipher_str != "no":
+
+# build class of colors to print color text
+
+    class colors:
+    
+        reset = '\033[0m'
+        bold = '\033[01m'
+        disable = '\033[02m'
+        underline = '\033[04m'
+        reverse = '\033[07m'
+        strikethrough = '\033[09m'
+        invisible = '\033[08m'
+
+    class fg:
+
+        black = '\033[30m'
+        red = '\033[31m'
+        green = '\033[32m'
+        orange = '\033[33m'
+        blue = '\033[34m'
+        purple = '\033[35m'
+        cyan = '\033[36m'
+        lightgrey = '\033[37m'
+        darkgrey = '\033[90m'
+        lightred = '\033[91m'
+        lightgreen = '\033[92m'
+        yellow = '\033[93m'
+        lightblue = '\033[94m'
+        pink = '\033[95m'
+        lightcyan = '\033[96m'
+
+                
+    name = input("\n\033[01mPlease enter your name: ")
+    kawaii = ("₍ ᐢ.ˬ.ᐢ₎")
+    import time
+    time.sleep(3)
+
+    import pyfiglet
+    name_art = pyfiglet.figlet_format(name, font='banner3-D')
+
+    kawaii = ("₍ ᐢ.ˬ.ᐢ₎")
+    
+    print("Hi.......\n\n\n" + colors.underline, name_art)
+    print("\n\n ══✿══╡°˖ This is the Vigenere Cipher! ✧˖°╞══✿══")
+
+   
+
+    # convert letter to index and index to letter
+
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+    letter_to_index = dict(zip(alphabet, range(len(alphabet))))
+    index_to_letter = dict(zip(range(len(alphabet)), alphabet))
+
+    def encrypt (message, key):
+        encrypted = ""
+
+        # split the message to the length of the key
+        split_message = [message[i:i + len(key)] for i in range (0, len(message), len(key))] # start, end, step
+
+        # convert message to index and add key
+        for each_split in split_message:
+            i = 0
+            for letter in each_split:
+                number = (letter_to_index[letter] + letter_to_index[key[i]]) % len(alphabet)
+                encrypted += index_to_letter[number]
+                i += 1
+        
+        return encrypted 
+
+
+    def decrypt (cipher, key):
+        decrypted = ""
+
+        # split ciphertext to the length of key
+        split_cipher = [cipher[i:i + len(key)] for i in range(0, len(cipher), len(key))] # start, end, step
+
+        # convert ciphertext to index and subtract key 
+        for each_split in split_cipher:
+            i = 0
+            for letter in each_split:
+                number = (letter_to_index[letter] - letter_to_index[key[i]]) % len(alphabet)
+                decrypted += index_to_letter[number]
+                i += 1
+        
+        return decrypted
+
+    def main ():
+        key = "knights"
+        message = "thisisthelasttaskhoorday"
+        encrypted_message = encrypt(message, key)
+
+        import time
+        time.sleep(3)
+        print("\n  █▒▒▒▒▒▒▒▒▒")
+        time.sleep(1)
+        print("\n  ███▒▒▒▒▒▒▒")
+        time.sleep(1)
+        print("\n  █████▒▒▒▒▒")
+        time.sleep(1)
+        print("\n  ███████▒▒▒")
+        time.sleep(1)
+        print("\n  ██████████")
+
+        print("\n\033[33m• • • • • • • The Message is: • • • • • • • " + "\n\n\t" + message.upper())
+        time.sleep(3)
+        print("\n\033[93m• • • • • • • The Ciphertext will be: • • • • • • • " + "\n\n\t" + encrypted_message.upper())
+    
+    main()
+
+    # looping
+    time.sleep(3)
+    cipher_str = input("\n\n\033[35mDo you want to try the Vigenere Cipher again? \n\nPlease enter YES or NO: ")
+    if cipher_str == "YES":
+        print("\n\033[36mSTARTING..........")
+        time.sleep(3)
+        print("\n  █▒▒▒▒▒▒▒▒▒")
+        time.sleep(1)
+        print("\n  ███▒▒▒▒▒▒▒")
+        time.sleep(1)
+        print("\n  █████▒▒▒▒▒")
+        time.sleep(1)
+        print("\n  ███████▒▒▒")
+        time.sleep(1)
+        print("\n  ██████████")
+        import time
+        time.sleep(3)
+    elif cipher_str == "NO":
+        import time
+        time.sleep(3)
+        print("\n ❁ ≖≖≖≖≖≖✿ END ❁ ≖≖≖≖≖≖ ❁")
+
+
+        # import pyfiglet module
+        import pyfiglet
+        outro = pyfiglet.figlet_format("Thank you!")
+        print("\n" + outro)
+        break
+
+    else:
+        print("INVALID INPUT.")
+
+
